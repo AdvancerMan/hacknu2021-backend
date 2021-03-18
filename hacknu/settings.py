d5b674d0437d3a9d26b65530664645ecd3843b8f
Kazakov Mihail <advancerman@gmail.com>
2021-03-18 17:27:13 +0300

    Added CORS support

--- hacknu/settings.py
+++ hacknu/settings.py
@@ -41,18 +41,24 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'testapp',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://admiring-pike-877d30.netlify.app/",
 ]
 
 ROOT_URLCONF = 'hacknu.urls'
