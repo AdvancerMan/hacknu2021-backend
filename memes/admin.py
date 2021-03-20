80cf30ad10d93f11a75c092bc1d18bfad6aa17ef
Kazakov Mihail <advancerman@gmail.com>
2021-03-20 10:24:11 +0300

    Registered models on admin site

--- memes/admin.py
+++ memes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.CardsUser)
+admin.site.register(models.CardDesign)
+admin.site.register(models.Card)
+admin.site.register(models.Battle)
